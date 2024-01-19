aae87b5326eac53c98776804df12c74b7bf00762
Roman <vvv@gmail.com>
2024-01-19 16:34:33 +0200

    Add Manager

---
+++ 1234/Manager.swift
@@ -0,0 +1,26 @@
+//
+//  Manager.swift
+//  1234
+//
+//  Created by Roman Verba on 15.01.2024.
+//
+
+import Foundation
+
+class JsonRepo {
+  
+  func load(completion: @escaping (Welcome) -> Void) {
+    let urlString =  "https://www.dropbox.com/scl/fi/bx467tupe3nbrsf4kg42s/Programs.json?rlkey=xybrscxbwcgwyf0udyvm01k4r&dl=1"
+    guard let url = URL(string: urlString) else { return }
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      
+      let welcome = try! JSONDecoder().decode(Welcome.self, from: data!)
+      
+      DispatchQueue.main.async {
+        completion(welcome)
+      }
+      
+    }.resume()
+  }
+}
